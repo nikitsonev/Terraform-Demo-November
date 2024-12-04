8810813eab62fcc62469bb10e308dfd18ec92cdc
DATAPARKINC\tsonev <nikolai.tsonev@hubparking.com>
2024-12-04 11:02:48 +0200

    add backend block

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.11.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstorageniki"
+    container_name       = "taskboardstorageniki"
+    key                  = "terraform.tfstate"
+
+  }
 }
 
 provider "azurerm" {
